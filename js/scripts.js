ccfbb0a25c55e394aec80a7d713041ca19dcab78
kohichi horii <kohichi.horii@gmail.com>
2018-10-19 15:46:20 -0700

    Add var in js for interests and dev

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,37 @@
-$(document).ready(function() {
-  $("form#track").submit(function(event) {
-    event.preventDefault();
-    var nameinput = ($("#name").val());
-    $(".name").text(nameinput);
+var dev = $("input:radio[name=dev]:checked").val();
+var interests = $("#interests").val();
 
-    var emailInput = ($("#email").val());
-    $(".email").text(emailInput);
 
-    var interestsInput = ($("#interests").val());
-    $(".interests").text(interestsInput);
 
-    var devInput = $("input:radio[name=dev]:checked").val();
-    $(".dev").text(devInput);
 
-    var commentsInput = ($("#comments").val());
-    $(".comments").text(commentsInput);
 
-    var number1 = parseInt($("#input1").val());
-    var number2 = parseInt($("#input2").val());
-    var operator = $("input:radio[name=operator]:checked").val();
-    var result = add(number1, number2);
-    $("#output").text(result);
 
-    $("#track-modal").toggle();
-    event.preventDefault();
-  });
-});
+
+// $(document).ready(function() {
+//   $("form#track").submit(function(event) {
+//     event.preventDefault();
+//     var nameinput = ($("#name").val());
+//     $(".name").text(nameinput);
+//
+//     var emailInput = ($("#email").val());
+//     $(".email").text(emailInput);
+//
+//     var interestsInput = ($("#interests").val());
+//     $(".interests").text(interestsInput);
+//
+//     var devInput = $("input:radio[name=dev]:checked").val();
+//     $(".dev").text(devInput);
+//
+//     var commentsInput = ($("#comments").val());
+//     $(".comments").text(commentsInput);
+//
+//     var int1 = parseInt($("#interests").val());
+//
+//     if (int1 === "front-end") {
+//       result = ruby
+//     $("#output").text(result);
+//
+//     $("#track-modal").toggle();
+//     event.preventDefault();
+//   });
+// });
