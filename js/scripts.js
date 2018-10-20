dc5162d2911663c3a09d4c4c0e1744b621cd9670
kohichi horii <kohichi.horii@gmail.com>
2018-10-20 15:16:17 -0700

    Add result in html with track and email inserts

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,45 @@ $(document).ready(function() {
     event.preventDefault();
 
     var name = $("#name").val();
+    var email = $("#email").val();
     var interests = $("#interests").val();
     var dev = $("#dev").val();
 
     if (interests === "front-end" && dev === "software") {
       $("#results").show()
       $("#nameInsert").append(name)
+      $("#emailInsert").append(email)
       $("#tracks").text("Ruby/Rails")
     }
     else if (interests === "front-end" && dev === "app") {
       $("#results").show()
       $("#nameInsert").append(name)
-      $("#tracks").text("Java/Android")
+      $("#emailInsert").append(email)
+      $("#tracks").text("Java")
+    }
+    else if (interests === "front-end" && dev === "webite") {
+      $("#results").show()
+      $("#nameInsert").append(name)
+      $("#emailInsert").append(email)
+      $("#tracks").text("CSS")
+    }
+    else if (interests === "back-end" && dev === "software") {
+      $("#results").show()
+      $("#nameInsert").append(name)
+      $("#emailInsert").append(email)
+      $("#tracks").text("Ruby/Rails")
+    }
+    else if (interests === "back-end" && dev === "app") {
+      $("#results").show()
+      $("#nameInsert").append(name)
+      $("#emailInsert").append(email)
+      $("#tracks").text("Java")
+    }
+    else if (interests === "back-end" && dev === "website") {
+      $("#results").show()
+      $("#nameInsert").append(name)
+      $("#emailInsert").append(email)
+      $("#tracks").text("CSS")
     }
   });
 });
